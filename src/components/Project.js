55c0cb27c701fd81fb48f90c3b2e42ec9d4d94e4
Carl Benbow <cbenbow@synergisticagility.com>
2021-07-17 22:09:37 -0400

    updated the project design for each card

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,25 @@
 import "../styles/Project.css";
 
 function Project() {
+
   return (
     <div>
       <div className="row">
-        <div className="col card cd-left cd-one"></div>
-        <div className="col card cd-right"></div>
+        <div className="col cd-left cd-one" id="cardBdOne">
+        <h2 className ="cd-one-text-title-1" type="button">Deployed Application</h2>
+          <a className ="cd-one-text" href="https://synag.github.io/project_1_recipe_mixer/index.html" type="button">Recipe Mixer</a>
+          <h2 className ="cd-one-text-title-2"  type="button">Github</h2>
+          <a id="gitOne" href="https://github.com/synag/project_1_recipe_mixer" type="button" className="fab fa-github cd-one-i"></a>
+        </div>
+        <div className="col  cd-right cd-two" id="cardBdTwo"></div>
       </div>
       <div className="row">
-        <div className="col card cd-left"></div>
-        <div className="col card cd-right"></div>
+        <div className="col  cd-left cd-three" id="cardBdThree"></div>
+        <div className="col  cd-right cd-four" id="cardBdFour"></div>
       </div>
       <div className="row">
-        <div className="col card cd-left"></div>
-        <div className="col card cd-right"></div>
+        <div className="col  cd-left cd-five" id="cardBdFive"></div>
+        <div className="col  cd-right cd-six" id="cardBdSix"></div>
       </div>
     </div>
   );
