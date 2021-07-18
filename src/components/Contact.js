ac7fa112038a4a1aa11dcc9d1982c8f672576f40
Carl Benbow <cbenbow@synergisticagility.com>
2021-07-18 13:49:42 -0400

    created contact page and styled projects page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,42 @@
-
 function Contact() {
-    return (
-      <main>
-        <h1>Example</h1>
-  
-      
-      </main>
-    );
-  }
-  
-  export default Contact;
+  return (
+    <main className="container">
+      <h1>Contact Me</h1>
+      <div className="mb-3">
+        <label  className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput"
+          placeholder="Name"
+        ></input>
+      </div>
+      <div className="mb-3">
+        <label  className="form-label">
+          Email
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Email"
+        ></input>
+    </div>
+    <div className="mb-3">
+        <label  className="form-label">
+          Message
+        </label>
+        <textarea
+          type="text"
+          className="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Message"
+        ></textarea>
+      </div>
+    </main>
+  );
+}
+
+export default Contact;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,157 @@
 import "../styles/Project.css";
 
 function Project() {
-
   return (
     <div>
       <div className="row">
         <div className="col cd-left cd-one" id="cardBdOne">
-        <h2 className ="cd-one-text-title-1" type="button">Deployed Application</h2>
-          <a className ="cd-one-text" href="https://synag.github.io/project_1_recipe_mixer/index.html" type="button">Recipe Mixer</a>
-          <h2 className ="cd-one-text-title-2"  type="button">Github</h2>
-          <a id="gitOne" href="https://github.com/synag/project_1_recipe_mixer" type="button" className="fab fa-github cd-one-i"></a>
+          <h2 className="cd-text-title-1" type="button">
+            Deployed Application
+          </h2>
+          <a
+            className="cd-text"
+            href="https://synag.github.io/project_1_recipe_mixer/index.html"
+            type="button"
+          >
+            Recipe Mixer
+          </a>
+          <h2 className="cd-text-title-2" type="button">
+            Github
+          </h2>
+          <a
+            id="gitOne"
+            href="https://github.com/synag/project_1_recipe_mixer"
+            type="button"
+            className="fab fa-github cd-i"
+          ></a>
+          <h2 className="cd-text-technologies">
+            Technologies: HTML, CSS, JS, Third Party APIs 
+          </h2>
         </div>
-        <div className="col  cd-right cd-two" id="cardBdTwo"></div>
+        <div className="col  cd-right cd-two" id="cardBdTwo">
+          <h2 className="cd-text-title-1" type="button">
+            Deployed Application
+          </h2>
+          <a
+            className="cd-text"
+            href="https://synag.github.io/day_planner/"
+            type="button"
+          >
+            Day Planner
+          </a>
+          <h2 className="cd-text-title-2" type="button">
+            Github
+          </h2>
+          <a
+            id="gitOne"
+            href="https://github.com/synag/day_planner"
+            type="button"
+            className="fab fa-github cd-i"
+          ></a>
+          <h2 className="cd-text-technologies">
+            Technologies: HTML, JS, CSS, Local Storage
+          </h2>
+        </div>
       </div>
       <div className="row">
-        <div className="col  cd-left cd-three" id="cardBdThree"></div>
-        <div className="col  cd-right cd-four" id="cardBdFour"></div>
+        <div className="col  cd-left cd-three" id="cardBdThree">
+          <h2 className="cd-text-title-1" type="button">
+            Deployed Application
+          </h2>
+          <a
+            className="cd-text"
+            href="https://fierce-plains-53758.herokuapp.com/"
+            type="button"
+          >
+            Tech Blog
+          </a>
+          <h2 className="cd-text-title-2" type="button">
+            Github
+          </h2>
+          <a
+            id="gitOne"
+            href="https://github.com/synag/tech-blog"
+            type="button"
+            className="fab fa-github cd-i"
+          ></a>
+          <h2 className="cd-text-technologies">
+            Technologies: HTML, JS, CSS, Bcrypt, Connect-session-sequelize, Dotenv, Express-handlebars, Mysql2, Node-fetch, Path, and Sequelize 
+          </h2>
+        </div>
+        <div className="col  cd-right cd-four" id="cardBdFour">
+          <h2 className="cd-text-title-1" type="button">
+            Deployed Application
+          </h2>
+          <a
+            className="cd-text"
+            href="https://safe-temple-93958.herokuapp.com"
+            type="button"
+          >
+            Fitness Tracker
+          </a>
+          <h2 className="cd-text-title-2" type="button">
+            Github
+          </h2>
+          <a
+            id="gitOne"
+            href="https://github.com/synag/fitness-tracker"
+            type="button"
+            className="fab fa-github cd-i"
+          ></a>
+          <h2 className="cd-text-technologies">
+            Technologies: HTML, JS, CSS, MongoDB, Mongoose, Express, Morgan
+          </h2>
+        </div>
       </div>
       <div className="row">
-        <div className="col  cd-left cd-five" id="cardBdFive"></div>
-        <div className="col  cd-right cd-six" id="cardBdSix"></div>
+        <div className="col  cd-left cd-five" id="cardBdFive">
+          <h2 className="cd-text-title-1" type="button">
+            Deployed Application
+          </h2>
+          <a
+            className="cd-text"
+            href="https://pure-chamber-08412.herokuapp.com/"
+            type="button"
+          >
+            iPortfolio
+          </a>
+          <h2 className="cd-text-title-2" type="button">
+            Github
+          </h2>
+          <a
+            id="gitOne"
+            href="https://github.com/tylerf808/iPortfolio"
+            type="button"
+            className="fab fa-github cd-i"
+          ></a>
+          <h2 className="cd-text-technologies">
+            Technologies: HTML, CSS, JS, Node express, SQL, Handlebars, Bcrypt, Node-fetch request, Connect-session-sequelize, Polygon, Dotenv, Express-handlebars, Express-session, Bootstrap
+          </h2>
+        </div>
+        <div className="col  cd-right cd-six" id="cardBdSix">
+          <h2 className="cd-text-title-1" type="button">
+            Deployed Application
+          </h2>
+          <a
+            className="cd-text"
+            href="https://synag.github.io/weather-dashboard"
+            type="button"
+          >
+            Weather Dashboard
+          </a>
+          <h2 className="cd-text-title-2" type="button">
+            Github
+          </h2>
+          <a
+            id="gitOne"
+            href="https://github.com/synag/weather-dashboard"
+            type="button"
+            className="fab fa-github cd-i"
+          ></a>
+          <h2 className="cd-text-technologies">
+            Technologies: HTML, CSS, JS, Jquery 
+          </h2>
+        </div>
       </div>
     </div>
   );
